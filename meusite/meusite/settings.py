5d661144c632d03769a77dfc1665c33843efa7e6
vSteps <victor.passos@escolar.ifrn.edu.br>
2024-07-29 11:32:38 +0000

    Projeto Conclúido

--- meusite/meusite/settings.py
+++ meusite/meusite/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-#STATIC_ROOT = '/home/victorsteps/meusite/static'
+STATIC_ROOT = '/home/victorsteps/meusite/static'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
